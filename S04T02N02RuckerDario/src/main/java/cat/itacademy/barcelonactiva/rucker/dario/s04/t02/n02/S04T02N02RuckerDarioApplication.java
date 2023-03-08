1235bf904c1f881fa45bb18a24bdb0e2f0b412d8
Dario <dariomsn@yahoo.com.ar>
2023-03-08 16:26:25 -0300

    commit

--- S04T02N02RuckerDario/src/main/java/cat/itacademy/barcelonactiva/rucker/dario/s04/t02/n02/S04T02N02RuckerDarioApplication.java
+++ S04T02N02RuckerDario/src/main/java/cat/itacademy/barcelonactiva/rucker/dario/s04/t02/n02/S04T02N02RuckerDarioApplication.java
@@ -1,14 +1,15 @@
 package cat.itacademy.barcelonactiva.rucker.dario.s04.t02.n02;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info (title = "Level 2 SQL", version = "1.0.0")
-		servers = {@Server(url = "http://localhost:8080"), @Server(url = "http://localhost:9000")}
-)
+//@OpenAPIDefinition(
+//		//info = @info(title = "Level 2 SQL", version = "1.0.0"),
+//		servers = {@Server(url = "http://localhost:8080"), @Server(url = "http://localhost:9000")}
+//)
 public class S04T02N02RuckerDarioApplication {
 
 	public static void main(String[] args) {
